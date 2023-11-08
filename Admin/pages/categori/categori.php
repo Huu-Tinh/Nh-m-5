60b020a26e0946e87c2126c1a90a941faca92b3e
Huu-Tinh <134404851+Huu-Tinh@users.noreply.github.com>
2023-11-08 23:12:07 +0700

    categori

--- Admin/pages/categori/categori.php
+++ Admin/pages/categori/categori.php
@@ -1,26 +1,27 @@
-<?php	
-class categori {
+<?php
+class categori
+{
    var $name_ct  = null;
-  
+   var $note = null;
    function getcategori()
    {
       $db = new connect();
       $select = "select * from categories";
       return $db->pdo_query($select);
    }
-  
-    function add($name_ct ,)
+
+   function add($name_ct,$note)
    {
       $db = new connect();
-      $query = "INSERT INTO categories(name_ct ,`soluong`) VALUES ('$name_ct ')";
+      $query = "INSERT INTO categories(name_ct ,`note`) VALUES ('$name_ct','$note')";
       $result = $db->pdo_execute($query);
       return $result;
    }
-   public function update($name_ct )
-    {
-        $db = new connect();
-        $sql = "UPDATE categories SET  name_ct  = '$name_ct '  WHERE  = " ;
-        $result = $db->pdo_execute($sql);
-        return $result;
-    }
+   public function update($id, $name_ct)
+   {
+      $db = new connect();
+      $sql = "UPDATE categories SET  name_ct  = '$name_ct '  WHERE id_categori = $id";
+      $result = $db->pdo_execute($sql);
+      return $result;
+   }
 }
