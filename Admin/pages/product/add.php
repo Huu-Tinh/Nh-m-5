1c1131b145c47d845d4d5398e4d90cb36130fa8b
Huu-Tinh <134404851+Huu-Tinh@users.noreply.github.com>
2023-11-16 16:22:03 +0700

    edit form add

--- Admin/pages/product/add.php
+++ Admin/pages/product/add.php
@@ -20,45 +20,60 @@ if (isset($_POST['addproduct'])) {
         <h5 class="card-title fw-semibold mb-4">Thêm mới sản phẩm</h5>
         <div class="card">
             <div class="card-body">
-                <form method="post">
-                    <div class="mb-3">
+                <form method="post" class="row">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">Tên sản phẩm</label>
-                        <input type="email" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp">
-
+                        <input type="text" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">Giá</label>
-                        <input type="email" class="form-control" name="price" id="exampleInputEmail1" aria-describedby="emailHelp">
-
+                        <input type="number" class="form-control" name="price" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">ảnh</label>
                         <input type="file" class="form-control" name="img" id="exampleInputEmail1" aria-describedby="emailHelp">
-
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">ảnh-1</label>
                         <input type="file" class="form-control" name="img_1" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">ảnh-2</label>
                         <input type="file" class="form-control" name="img_2" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="exampleInputEmail1" class="form-label">ảnh-3</label>
                         <input type="file" class="form-control" name="img_3" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
 
-                    <div class="mb-3">
+                    <div class="mb-3 col-md-6">
                         <label for="exampleInputPassword1" class="form-label">Số lượng</label>
-                        <input type="number" class="form-control" name="Quantity" id="exampleInputPassword1">
+                        <input type="number" class="form-control" name="quantity" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 col-md-6">
+                        <label for="" class="form-label">Loại</label>
+                        <select class="form-select" name="category_id" aria-label=".form-select-sm example">
+                            <?php
+                            $categori = new categori();
+                            $db = new connect();
+                            $select = $categori->getCategori();
+
+                            foreach ($select as $data) {
+                                echo '
+                                    <option value="' . $data['id_categori'] . '">
+                                        ' . $data['name_ct'] . '
+                                    </option>';
+                            }
+                            ?>
+
+                        </select>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Mô tả</label>
                         <textarea type="text" class="form-control" name="note" id=""></textarea>
                     </div>
-                  
-                    <button type="submit" name="addproduct" class="btn btn-primary">Thêm</button>
+
+                    <button type="submit" name="addproduct" class="btn btn-primary col-1">Thêm</button>
                 </form>
             </div>
         </div>
