4eb350bcb5d3c977ecc0462b12399d044e51fad3
Huu-Tinh <134404851+Huu-Tinh@users.noreply.github.com>
2023-11-16 16:40:36 +0700

    Update sidebar.php

--- Admin/includes/sidebar.php
+++ Admin/includes/sidebar.php
@@ -66,39 +66,19 @@
                     </a>
                 </li>
 
-                <li class="nav-small-cap" style="margin-top: 235px;">
+                <li class="nav-small-cap" style="margin-top: 255px;">
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                    <!-- <span class="hide-menu">AUTH</span> -->
+                    <span class="hide-menu">AUTH</span>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="index.php?act=login" aria-expanded="false">
                         <span>
                             <i class="ti ti-login"></i>
                         </span>
-                        <span class="hide-menu">Login</span>
+                        <span class="hide-menu">Logout</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="includes/register.php" aria-expanded="false">
-                        <span>
-                            <i class="ti ti-user-plus"></i>
-                        </span>
-                        <span class="hide-menu">Register</span>
-                    </a>
-                </li>
-
             </ul>
-            <!-- <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
-                <div class="d-flex">
-                    <div class="unlimited-access-title me-3">
-                        <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
-                        <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/" target="_blank" class="btn btn-primary fs-2 fw-semibold lh-sm">Buy Pro</a>
-                    </div>
-                    <div class="unlimited-access-img">
-                        <img src="../assets/images/backgrounds/rocket.png" alt="" class="img-fluid">
-                    </div>
-                </div>
-            </div> -->
         </nav>
         <!-- End Sidebar navigation -->
     </div>
