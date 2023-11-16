93232cb27754918027a2007c22e67fe51d72da74
Huu-Tinh <134404851+Huu-Tinh@users.noreply.github.com>
2023-11-16 16:22:20 +0700

    fix lỗi add

--- Admin/pages/product/product.php
+++ Admin/pages/product/product.php
@@ -24,14 +24,14 @@ class product {
     function add($name, $price, $img, $img_1, $img_2, $img_3, $note,$quantity)
    {
       $db = new connect();
-      $query = "INSERT INTO products(`name`,`price`,img,img_1,`img_2`,img_3, note,quantity) VALUES(`$name`,`$price`,$img,$img_1,`$img_2`,$img_3, $note,$quantity)";
+      $query = "INSERT INTO products(`name_pr`,`price`,img,img_1,`img_2`,img_3, note,quantity) VALUES('$name','$price','$img','$img_1','$img_2','$img_3', '$note','$quantity')";
       $result = $db->pdo_execute($query);
       return $result;
    }
    public function update($id_product,$name, $price, $img, $img_1, $img_2, $img_3, $note,$quantity)
     {
         $db = new connect();
-        $sql = "UPDATE products SET  name = '$name', `price` = '$price', img = '$img', img_1 = '$img_1',img_2 = '$img_2', `img_3` ='$img_3', note = '$note', quantity ='$quantity'  WHERE id_product = " . $id_product;
+        $sql = "UPDATE products SET  name_pr = '$name', `price` = '$price', img = '$img', img_1 = '$img_1',img_2 = '$img_2', `img_3` ='$img_3', note = '$note', quantity ='$quantity'  WHERE id_product = " . $id_product;
         $result = $db->pdo_execute($sql);
         return $result;
     }
