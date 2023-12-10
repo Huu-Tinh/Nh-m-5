4299ecc9d2c73de74044c274a5e7041b5aaf27e7
Huu-Tinh <134404851+Huu-Tinh@users.noreply.github.com>
2023-12-11 01:20:46 +0700

    update product

--- User/pages/shop.php
+++ User/pages/shop.php
@@ -23,187 +23,118 @@ $product = new product();
 <!-- Start Content -->
 <div class="container py-5">
     <div class="row">
-
+        <!-- Phân loại sp  -->
         <div class="col-lg-3">
-            <h1 class="h2 pb-4">Categories</h1>
-            <ul class="list-unstyled templatemo-accordion">
-                <li class="pb-3">
-                    <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
-                        Gender
-                        <i class="fa fa-fw fa-chevron-circle-down mt-1"></i>
-                    </a>
-                    <ul class="collapse show list-unstyled pl-3">
-                        <li><a class="text-decoration-none" href="#">Men</a></li>
-                        <li><a class="text-decoration-none" href="#">Women</a></li>
-                    </ul>
-                </li>
-                <li class="pb-3">
-                    <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
-                        Sale
-                        <i class="pull-right fa fa-fw fa-chevron-circle-down mt-1"></i>
-                    </a>
-                    <ul id="collapseTwo" class="collapse list-unstyled pl-3">
-                        <li><a class="text-decoration-none" href="#">Sport</a></li>
-                        <li><a class="text-decoration-none" href="#">Luxury</a></li>
-                    </ul>
-                </li>
-                <li class="pb-3">
-                    <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
-                        Product
-                        <i class="pull-right fa fa-fw fa-chevron-circle-down mt-1"></i>
-                    </a>
-                    <ul id="collapseThree" class="collapse list-unstyled pl-3">
-                        <li><a class="text-decoration-none" href="#">Bag</a></li>
-                        <li><a class="text-decoration-none" href="#">Sweather</a></li>
-                        <li><a class="text-decoration-none" href="#">Sunglass</a></li>
-                    </ul>
-                </li>
+            <h1 class="h2 pb-4">Danh mục sản phẩm</h1>
+            <ul class="list-inline shop-top-menu pb-3 pt-1">
+                <form action="" method="post">
+                    <li class="list-item">
+                        <button type="submit" name="all" value="1" class="btn border-0 bg-body p-0">All</button>
+                    </li>
+                    <?php
+                    $categori = new categori();
+                    $slCategori = $categori->getcategori();
+                    foreach ($slCategori as $datacategori) {
+                        echo '
+                    <li class="list-item">
+                        <button type="submit" name="categori" value="' . $datacategori['id_categori'] . '" class="btn border-0 bg-body p-0">' . $datacategori['name_ct'] . '</button>
+                     </li>
+                    ';
+                    }
+                    ?>
+                </form>
             </ul>
         </div>
 
         <div class="col-lg-9">
             <div class="row">
-                <div class="col-md-6">
-                    <ul class="list-inline shop-top-menu pb-3 pt-1">
-                        <li class="list-inline-item">
-                            <a class="h3 text-dark text-decoration-none mr-3" href="#">All</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="h3 text-dark text-decoration-none mr-3" href="#">Men's</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="h3 text-dark text-decoration-none" href="#">Women's</a>
-                        </li>
-                    </ul>
+                <div class="col-md-8">
                 </div>
-                <div class="col-md-6 pb-4">
-                    <div class="d-flex">
-                        <select class="form-control">
-                            <option>Featured</option>
-                            <option>A to Z</option>
-                            <option>Item</option>
-                        </select>
-                    </div>
+                <!-- Option Sắp xếp sp  -->
+                <div class="col-md-4 pb-4">
+                    <form action="" method="post">
+                        <div class="d-flex">
+                            <select name="role" class="form-select border border-end-0 rounded-0">
+                                <option value="0" <? if (isset($_POST['role']) && $_POST['role'] == 0) {
+                                                        echo 'selected';
+                                                    } ?>>Sắp xếp</option>
+                                <option value="1" <? if (isset($_POST['role']) && $_POST['role'] == 1) {
+                                                        echo 'selected';
+                                                    } ?>>A đến Z</option>
+                                <option value="2" <? if (isset($_POST['role']) && $_POST['role'] == 2) {
+                                                        echo 'selected';
+                                                    } ?>>Giá thấp đến cao</option>
+                                <option value="3" <? if (isset($_POST['role']) && $_POST['role'] == 3) {
+                                                        echo 'selected';
+                                                    } ?>>Giá cao đến thấp</option>
+                                <option value="4" <? if (isset($_POST['role']) && $_POST['role'] == 4) {
+                                                        echo 'selected';
+                                                    } ?>>Sản phẩm mới nhất</option>
+                                <option value="5" <? if (isset($_POST['role']) && $_POST['role'] == 5) {
+                                                        echo 'selected';
+                                                    } ?>>Sản phẩm cũ nhất</option>
+                            </select>
+                            <button type="submit" name="arrange" class="px-3 fs-5 btn bg-success text-white rounded-0"><i class="bi bi-funnel"></i></button>
+                        </div>
+                    </form>
                 </div>
             </div>
+
+            <!-- Xuất sp  -->
             <div class="row">
                 <?php
-                $product = new product();
-                $select = $product->getproduct();
+                $variable = 'all';
+                if (isset($_POST['search'])) {
+                    $variable  = 'search';
+                } elseif (isset($_POST['categori'])) {
+                    $variable  = 'categori';
+                } elseif (isset($_POST['all'])) {
+                    $variable = 'all';
+                } elseif (isset($_POST['arrange'])) {
+                    $variable = 'arrange';
+                }
+                switch ($variable) {
+                    case 'all':
+                        // Show tất cả sản phẩm 
+                        $select = $product->getproduct();
+                        break;
+                    case 'search':
+                        // Tìm kím sản phẩm 
+                        $namesearch = $_POST['search'];
+                        $select = $product->search($namesearch);
+                        break;
+                    case 'categori':
+                        // Tìm kím sp theo loại 
+                        $valuecategori = $_POST['categori'];
+                        $select = $product->getpr_categori($valuecategori);
+                        break;
+                    case 'arrange':
+                        // Sắp xếp sp 
+                        if ($_POST['role'] == 0) {
+                            $select = $product->getproduct();
+                        } elseif ($_POST['role'] == 1) {
+                            // Sắp xếp A-Z
+                            $select = $product->getpr_aToz();
+                        } elseif ($_POST['role'] == 2) {
+                            // Sắp xếp giá giảm dần 
+                            $select = $product->getpr_priceAsc();
+                        } elseif ($_POST['role'] == 3) {
+                            // Sắp xếp giá tăng dần 
+                            $select = $product->getpr_priceDesc();
+                        } elseif ($_POST['role'] == 4) {
+                            // Sắp xếp sp mới nhất 
+                            $select = $product->getpr_priceDesc();
+                        } elseif ($_POST['role'] == 5) {
+                            // Sắp xếp sp cũ nhất
+                            $select = $product->getpr_priceAsc();
+                        }
+                        break;
+                    default:
+                        $select = $product->getproduct();
+                        break;
+                }
                 foreach ($select as $value) {
-                    echo '
-                    <div class="col-md-4">
-                        <div class="card mb-4 product-wap rounded-0">
-                           
-                                <div class="card rounded-0">
-                                    <img class="card-img rounded-0 img-fluid" src="../Admin/assets/images/products/' . $value['img'] . '" style="height: 300px;">
-                                    <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
-                                        <ul class="list-unstyled">
-                                            <li><a class="btn btn-success text-white" href="shop-single.html"><i class="far fa-heart"></i></a></li>
-                                            <li><a class="btn btn-success text-white mt-2" href="index.php?act=shop-single&id_pr=' . $value['id_product'] . '"><i class="far fa-eye"></i></a></li>
-                                            <li><button type="button" class="btn btn-success text-white mt-2" href="index.php?act=cart" data-bs-toggle="modal" data-bs-target="#exampleModal' . $value['id_product'] . '"><i class="fas fa-cart-plus"></i></button></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <a href="index.php?act=shop-single&id_pr=' . $value['id_product'] . '" class="h3 text-decoration-none">' . $value['name_pr'] . '</a>
-                                    <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
-                                        <li>M/L/X/XL</li>
-                                        <li class="pt-2">
-                                            <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
-                                            <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
-                                            <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
-                                            <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
-                                            <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
-                                        </li>
-                                    </ul>
-                                    <ul class="list-unstyled d-flex justify-content-center mb-1">
-                                        <li>
-                                            <i class="text-warning fa fa-star"></i>
-                                            <i class="text-warning fa fa-star"></i>
-                                            <i class="text-warning fa fa-star"></i>
-                                            <i class="text-muted fa fa-star"></i>
-                                            <i class="text-muted fa fa-star"></i>
-                                        </li>
-                                    </ul>
-                                    <p class="text-center mb-0">' . number_format($value['price'], 0, ".", ".") . '</p>   
-                                </div>
-                        </div>
-                    </div>';
-                    echo '
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal' . $value['id_product'] . '" tabindex="-1" aria-labelledby="exampleModalLabel' . $value['id_product'] . '" aria-hidden="true">
-                        <div class="modal-dialog modal-lg modal-dialog-centered">
-                            <div class="modal-content">
-                                <form action="index.php?act=carts&get=toCart" method="post">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel' . $value['id_product'] . '">Thêm vào giỏ hàng</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div class="row">
-                                            <div class="row col-6">
-                                                <img class="col-1 card-img rounded-0 img-fluid" src="../Admin/assets/images/products/' . $value['img'] . '">
-                                            </div>
-                                            <div class="col-6">
-                                                <h1 class="h2">' . $value['name_pr'] . '</h1>
-                                                <label>Giá:</label>
-                                                <label class="text-danger fw-bold mb-3">' . number_format($value['price'], 0, ".", ".") . ' vnd</label>
-                                                <div class="row">
-                                                    <div class="col-auto">
-                                                        <ul class="list-inline pb-3">
-                                                            <li class="list-inline-item">Size :
-                                                            </li>
-                                                            <li class="list-inline-item">
-                                                                <input type="radio" value="38" class="btn-check" name="size" id="btn-1' . $value['id_product'] . '" autocomplete="off">
-                                                                <label class="btn btn-outline-success" for="btn-1' . $value['id_product'] . '">38</label>
-                                                            </li>
-                                                            <li class="list-inline-item">
-                                                                <input type="radio" value="39" class="btn-check" name="size" id="btn-2' . $value['id_product'] . '" autocomplete="off" checked>
-                                                                <label class="btn btn-outline-success" for="btn-2' . $value['id_product'] . '">39</label>
-                                                            </li>
-                                                            <li class="list-inline-item">
-                                                                <input type="radio" value="40" class="btn-check" name="size" id="btn-3' . $value['id_product'] . '" autocomplete="off">
-                                                                <label class="btn btn-outline-success" for="btn-3' . $value['id_product'] . '">40</label>
-                                                            </li>
-                                                            <li class="list-inline-item">
-                                                                <input type="radio" value="41" class="btn-check" name="size" id="btn-4' . $value['id_product'] . '" autocomplete="off">
-                                                                <label class="btn btn-outline-success" for="btn-4' . $value['id_product'] . '">41</label>
-                                                            </li>
-                                                            <li class="list-inline-item">
-                                                                <input type="radio" value="42" class="btn-check" name="size" id="btn-5' . $value['id_product'] . '" autocomplete="off">
-                                                                <label class="btn btn-outline-success" for="btn-5' . $value['id_product'] . '">42</label>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="col-auto">
-                                                        <ul class="list-inline pb-3">
-                                                            <li class="list-inline-item text-right">
-                                                            Quantity
-                                                            
-                                                            </li>
-                                                            <li class="list-inline-item"><button type="button" class="btn btn-success" onclick="decreaseNumber' . $value['id_product'] . '()">-</button></li>
-                                                            <li class="list-inline-item"><input class="text-center text-white border-0 outline-0 rounded bg-secondary" style="width:50px;" type="number" name="quantity" id="myNumber' . $value['id_product'] . '" value="1"></li>
-                                                            <li class="list-inline-item"><button type="button" class="btn btn-success" onclick="increaseNumber' . $value['id_product'] . '()">+</button></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="submit" name="addcart" class="btn btn-success">Thêm vào giỏ hàng</button>
-                                    </div>
-                                    <input type="hidden" name="id_product" value="' . $value['id_product'] . '">
-                                    <input type="hidden" name="name" value="' . $value['name_pr'] . '">
-                                    <input type="hidden" name="price" value="' . $value['price'] . '">
-                                    <input type="hidden" name="img" value="' . $value['img'] . '">
-                                  
-                                </form>
-                             </div>
-                        </div>
-                    </div>';
+                    include "./includes/listproduct.php";
                 }
                 ?>
             </div>
